d0280f214c62f0027bd55bdd2883a67f2ea20f03
Plamen Yovchev <plamenkooo@gmail.com>
2020-07-24 15:37:53 +0300

    Implement action and reducer

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('Welcome from index.js');
+// action types
+const actionTypes = {
+    BUY_CAR: "BUY_CAR"
+};
+
+// action
+const buyCar = () => {
+    return {
+        type: actionTypes.BUY_CAR,
+        info: 'First redux action'
+    };
+};
+
+// state 
+const carShopInitialState = {
+    availableCars: 100
+};
+
+// reducer
+const reducer = (state = carShopInitialState, action) => {
+    switch (action.type) {
+        case actionTypes.BUY_CAR:
+            return {
+                ...state,
+                availableCars: availableCars - 1
+            };
+        default:
+            return state;
+    }
+};
