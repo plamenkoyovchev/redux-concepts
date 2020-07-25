8c1435b4b9ffe81c6563ac7ea3034cd4414d311a
plamenyovchev <plamenkooo@gmail.com>
2020-07-25 12:54:49 +0300

    setup async actions

---
+++ asyncActions.js
@@ -0,0 +1,61 @@
+const redux = require('redux');
+const createStore = redux.createStore;
+
+const initialState = {
+    loading: false,
+    users: [],
+    error: ''
+};
+
+const actionTypes = {
+    FETCH_USERS_START: 'FETCH_USERS_START',
+    FETCH_USERS_SUCCESS: 'FETCH_USERS_SUCCESS',
+    FETCH_USERS_FAILURE: 'FETCH_USERS_FAILURE'
+};
+
+const fetchUsers = () => {
+    return {
+        type: actionTypes.FETCH_USERS_START
+    };
+};
+
+const fetchUsersSuccess = (users) => {
+    return {
+        type: actionTypes.FETCH_USERS_SUCCESS,
+        payload: users
+    };
+};
+
+const fetchUsersFalure = (error) => {
+    return {
+        type: actionTypes.FETCH_USERS_FAILURE,
+        payload: error
+    };
+};
+
+const reducer = (state = initialState, action) => {
+    switch (state) {
+        case actionTypes.FETCH_USERS_START:
+            return {
+                ...state,
+                loading: true
+            };
+        case actionTypes.FETCH_USERS_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                users: action.payload,
+                error: ''
+            };
+        case actionTypes.FETCH_USERS_FAILURE:
+            return {
+                ...state,
+                loading: false,
+                error: action.payload
+            };
+        default:
+            return state;
+    };
+};
+
+const store = createStore(reducer);
